201e8b2018751280ab9c0b71e35ce15038a76081
malki-cedheq <malkicedheq@gmail.com>
2023-11-09 11:16:30 -0300

    change for test propose

--- publisher_mongo.py
+++ publisher_mongo.py
@@ -1,11 +1,11 @@
 # publisher_mongo.py
 # par com subscriber_mongo.py
-'''
+"""
 Autor: Malki-çedheq
 Descrição: publica um JSON MONGODB no broker mqtt
 Data: 11/02/2023
 Atualizado: 19/06/2023
-'''
+"""
 from dotenv import dotenv_values
 import random
 import time
@@ -15,18 +15,19 @@ import json
 
 env = dotenv_values(".env")
 
-BROKER = env['BROKER']
-PORT = int(env['PORT'])
-USERNAME = env['USERNAME']
-PASSWORD = env['PASSWORD']
-PROTOCOL = 'tcp'  # tcp / websockets
-CLIENT_ID = f'python-mqtt-{random.randint(0, 100)}'
+BROKER = env["BROKER"]
+PORT = int(env["PORT"])
+USERNAME = env["USERNAME"]
+PASSWORD = env["PASSWORD"]
+PROTOCOL = "tcp"  # tcp / websockets
+CLIENT_ID = f"python-mqtt-{random.randint(0, 100)}"
 
 
 def connect_mqtt() -> mqtt_client:
-    '''
+    """
     Conecta o cliente mqtt ao BROKER mqtt
-    '''
+    """
+
     def on_connect(client, userdata, flags, rc):
         if rc == 0:
             print("Conectado ao BROKER MQTT com sucesso!")
@@ -41,18 +42,18 @@ def connect_mqtt() -> mqtt_client:
 
 
 def publish(client, topic: str, msg: str):
-    '''
+    """
     O cliente mqtt publica em um tópico no broker
     Argumentos:
     client: instância do cliente mqtt
     topic: nome do tópico para publicação
     msg: string a ser publicada
-    '''
+    """
     result = client.publish(topic, msg)
     status = result[0]  # result: [0, 1]
     if status == 0:
-        return (f"Enviado `{msg}` ao tópico `{topic}`")
-    return (f"Falha ao enviar mensagem ao tópico {topic}")
+        return f"Enviado `{msg}` ao tópico `{topic}`"
+    return f"Falha ao enviar mensagem ao tópico {topic}"
 
 
 def run():
@@ -63,64 +64,69 @@ def run():
         client.loop_start()
 
         msg_count = 0
+        cnt_temperatura = 31
         while True:
-            time.sleep(0.5)  # segundos
-
-            #teste envio ecg
+            time.sleep(1.0)  # segundos
+            # teste envio ecg
             ecg_array = []
             for i in range(50):
                 ecg_array.append(str(random.randint(0, 1000)))
             ecg_json_data = {
-                "id_paciente": '2',
-                "id_sessao": '2',
-                "id_exercicio": '24',
-                "timestamp": datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+                "id_paciente": "2",
+                "id_sessao": "2",
+                "id_exercicio": "24",
+                "timestamp": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
                 "n_pacote": str(msg_count),
                 "ecg": ecg_array,
             }
             msg = json.dumps(ecg_json_data)
-            result = publish(client, topic='sismo01/ecg', msg=msg)
+            result = publish(client, topic="sismo01/ecg", msg=msg)
 
-            #teste envio bpm
+            # teste envio bpm
             bpm_json_data = {
-                "id_paciente": '2',
-                "id_sessao": '2',
-                "id_exercicio": '24',
-                "timestamp": datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+                "id_paciente": "2",
+                "id_sessao": "2",
+                "id_exercicio": "24",
+                "timestamp": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
                 "n_pacote": str(msg_count),
                 "bpm": str(random.randint(60, 100)),
             }
             msg = json.dumps(bpm_json_data)
-            result = publish(client, topic='sismo01/bpm', msg=msg)
+            result = publish(client, topic="sismo01/bpm", msg=msg)
 
-            #teste envio spo2
+            # teste envio spo2
             spo2_json_data = {
-                "id_paciente": '2',
-                "id_sessao": '2',
-                "id_exercicio": '24',
-                "timestamp": datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+                "id_paciente": "2",
+                "id_sessao": "2",
+                "id_exercicio": "24",
+                "timestamp": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
                 "n_pacote": str(msg_count),
                 "spo2": str(random.randint(92, 110)),
             }
             msg = json.dumps(spo2_json_data)
-            result = publish(client, topic='sismo01/spo2', msg=msg)
+            result = publish(client, topic="sismo01/spo2", msg=msg)
 
-            #teste envio temperatura
+            # teste envio temperatura
             temperatura_json_data = {
-                "id_paciente": '2',
-                "id_sessao": '2',
-                "id_exercicio": '24',
-                "timestamp": datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+                "id_paciente": "2",
+                "id_sessao": "2",
+                "id_exercicio": "24",
+                "timestamp": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
                 "n_pacote": str(msg_count),
-                "temperatura": str(random.randint(34, 40)),
+                "temperatura": str(cnt_temperatura),  # str(random.randint(34, 40)),
             }
             msg = json.dumps(temperatura_json_data)
-            result = publish(client, topic='sismo01/temperatura', msg=msg)
-            
-            #print(result)
+            result = publish(client, topic="sismo01/temperatura", msg=msg)
+
+            # print(result)
             msg_count += 1
+            if cnt_temperatura > 43:
+                cnt_temperatura = 31
+            else:
+                cnt_temperatura += 1
     else:
         print("Não conectou ao broker MQTT!")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     run()
--- subscriber_mongo.py
+++ subscriber_mongo.py
@@ -1,10 +1,10 @@
 # subscriber_mongo.py
 # par com publisher_mongo.py
-'''
+"""
 Autor: Malki-çedheq
 Descrição: inscreve no BROKER mqtt, e envia JSON para MONGODB
 Data: 11/02/2023
-'''
+"""
 from dotenv import dotenv_values
 import random
 from paho.mqtt import client as mqtt_client
@@ -14,21 +14,21 @@ import json
 env = dotenv_values(".env")
 
 # instância do PyMongo
-mongoClient = MongoClient(env['MONGO_URI'])
+mongoClient = MongoClient(env["MONGO_URI"])
 
-BROKER = env['BROKER']
-PORT = int(env['PORT'])
-USERNAME = env['USERNAME']
-PASSWORD = env['PASSWORD']
-PROTOCOL = 'tcp'  # tcp / websockets
-CLIENT_ID = 'python-mqtt-{}'.format(random.randint(0, 100))
-
+BROKER = env["BROKER"]
+PORT = int(env["PORT"])
+USERNAME = env["USERNAME"]
+PASSWORD = env["PASSWORD"]
+PROTOCOL = "tcp"  # tcp / websockets
+CLIENT_ID = "python-mqtt-{}".format(random.randint(0, 100))
 
 
 def connect_mqtt() -> mqtt_client:
-    '''
+    """
     Conecta o cliente mqtt ao BROKER mqtt
-    '''
+    """
+
     def on_connect(client, userdata, flags, rc):
         if rc == 0:
             print("Conectado ao BROKER MQTT com sucesso!")
@@ -43,31 +43,33 @@ def connect_mqtt() -> mqtt_client:
 
 
 def subscribe(client: mqtt_client, database: str, topic: str):
-    '''
+    """
     Inscrive o cliente mqtt em um tópico
     Argumentos:
     client: instância do cliente mqtt
     topic: nome do tópico para inscrição
-    '''
+    """
     instancia, colecao = topic.split("/")
 
     def on_message(client, userdata, msg):
         instancia, colecao = msg.topic.split("/")
-        print(f"Recebeu `{msg.payload.decode()}` do tópico `{msg.topic}` -> coleção `{colecao}`")
-        send_to_mongo(msg.payload.decode(), database, colecao)
+        print(
+            f"Recebeu `{msg.payload.decode()}` do tópico `{msg.topic}` -> coleção `{colecao}`"
+        )
+        # send_to_mongo(msg.payload.decode(), database, colecao)
 
     client.subscribe(topic)
     client.on_message = on_message
 
 
 def send_to_mongo(payload: str, database: str, collection: str) -> None:
-    '''
+    """
     Decodifica o Payload como JSON e insere o documento no MongoDB
     Argumentos:
     payload: os dados JSON codificados em string
     database: o db no mongoDB que irá receber o documento
     collection: a coleção no mongoDB que irá receber o documento
-    '''
+    """
     db = mongoClient[database]
     db[collection].insert_one(json.loads(payload))
 
@@ -76,16 +78,16 @@ def run():
     client = connect_mqtt()  # conecta ao broker
 
     # Inscrições nos tópicos
-    subscribe(client, 'sismo', 'sismo01/temperatura')
-    subscribe(client, 'sismo', 'sismo01/bpm')
-    subscribe(client, 'sismo', 'sismo01/spo2')
-    subscribe(client, 'sismo', 'sismo01/ecg')
-    subscribe(client, 'sismo', 'sismo01/acelerometria')
-    subscribe(client, 'sismo', 'sismo01/giroscopia')
+    subscribe(client, "sismo", "sismo01/temperatura")
+    subscribe(client, "sismo", "sismo01/bpm")
+    subscribe(client, "sismo", "sismo01/spo2")
+    subscribe(client, "sismo", "sismo01/ecg")
+    subscribe(client, "sismo", "sismo01/acelerometria")
+    subscribe(client, "sismo", "sismo01/giroscopia")
 
     # O método bloqueia o programa, é útil quando o programa deve ser executado indefinidamente
     client.loop_forever()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     run()
